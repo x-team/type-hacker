a47664c51ac5829f7b7a657e1ed1f9c5c7666652
Bernardo Rubín <bernardorubin@gmail.com>
2022-05-20 13:34:28 -0500

    Improve combo bar size and font sizes

--- src/scenes/ScoreScene/index.ts
+++ src/scenes/ScoreScene/index.ts
@@ -11,6 +11,7 @@ import TFBaseScene from '../TFBaseScene';
 
 const circularScoreBarMeasurements = { x: 985, y: 184, radius: 90 };
 const horizontalComboBarMeasurements = { x: 735, y: 942 };
+const isMobile = checkIfMobile();
 
 export default class ScoreScene extends TFBaseScene {
   static RED_COMBO_BAR_COLOR = 0xff0000;
@@ -59,7 +60,7 @@ export default class ScoreScene extends TFBaseScene {
       graphics?.fillStyle(color, 1);
       const width = 480;
       const percent = Phaser.Math.Clamp(value, 0, 100) / 100;
-      graphics?.fillRect(0, 0, width * percent, 8);
+      graphics?.fillRect(0, 0, width * percent, isMobile ? 18 : 8);
       graphics.x = x;
       graphics.y = y;
       graphics.scaleX = isFullLength ? 1 : 0;
@@ -111,6 +112,8 @@ export default class ScoreScene extends TFBaseScene {
       true,
       '50px'
     );
+    isMobile && currentScoreMultiplier.setFontSize(70);
+    isMobile && currentScoreMultiplier.setPadding(30);
 
     const plusScoreWords = {
       left: new Word(this, 0, 0, '', '#FC9842', true, '30px', false),
@@ -129,7 +132,7 @@ export default class ScoreScene extends TFBaseScene {
     }) => {
       const guessText = monitorData.guessText!;
       const plusScoreWord = plusScoreWords[monitorData.name];
-      checkIfMobile() && plusScoreWord.setFontSize(90);
+      isMobile && plusScoreWord.setFontSize(90);
       plusScoreWord.setText(`+${plusScore}`);
       plusScoreWord.x = guessText.x + guessText.width / 2 + score.length * 15;
       plusScoreWord.y = guessText.y;
